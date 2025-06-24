8f19899158d6aae4a98398df8d696eac7d9b9ceb
RenatoPhys <renato.critelli.ifusp@gmail.com>
2025-06-24 11:53:46 -0300

    updates

---
+++ deploy/script_deploy_2.py
@@ -0,0 +1,5 @@
+from deployer.deploy import deploy_from_config
+
+# SÓ ISSO! Pega tudo do JSON automaticamente
+# Usa um arquivo específico
+deploy_from_config("../selected/combined_strategy_2.json", strategies_file="entries.py", wait_mode=True)
--- deploy/script_deploy_3.py
+++ deploy/script_deploy_3.py
@@ -2,4 +2,4 @@ from deployer.deploy import deploy_from_config
 
 # SÓ ISSO! Pega tudo do JSON automaticamente
 # Usa um arquivo específico
-deploy_from_config("combined_strategy_teste.json", strategies_file="entries.py", wait_mode=True)
+deploy_from_config("../selected/combined_strategy_3.json", strategies_file="entries.py", wait_mode=True)
